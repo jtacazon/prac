b26e344946e5bc5b9924f8183acc9010530c63c7
jtacazon <jtacazon@gmail.com>
2017-07-20 23:05:58 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,35 +1,8 @@
 def do_something():
-	a = 1
-	b = 2
-	c = 3
-	print (a+b)
-
-def do_something_else():
-	a = 2
-	b = 2
-	print (a*b)
-
-def hello_world():
-	print ("Hello World!")
-
-def sup_world():
-	print ("sup world")
-
-def goodbye_world():
-	print ("goodbye World!")
-
-def rofl():
-	print ("rofl")
-
-def lol():
-	print ("lol")
-	print ("lol")
-	print ("lol")
-	print ("lol")
+	
 	
 if __name__ == '__main__':
-	goodbye_world()
-	hello_world()
+	do_something()
 
 
 
