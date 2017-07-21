f420949cc80bd2311fad76625b2227120573a7ee
Tacazon <mli020@784f43a2f959.cof.ds.capitalone.com>
2017-07-20 22:07:11 -0400

    added goodbye world feature

--- test1.py
+++ test1.py
@@ -9,11 +9,13 @@ def do_something_else():
 	b = 2
 	print (a*b)
 
-	
 def hello_world():
 	print ("Hello World!")
 
+def goodbye_world():
+	print ("goodbye World!")
+
 if __name__ == '__main__':
-	hello_world()
+	goodbye_world()
 
 
